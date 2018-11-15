3fd80a38ecc187493b7695db4838a2e48dce5d7b
Natasha S Love <debora@inbox.ru>
2018-11-14 18:08:11 -0600

    initial

---
+++ BadSubscriptCaught.java
@@ -0,0 +1,19 @@
+import java.io.*; 
+import java.util.*; 
+
+public class BadSubscriptCaught {
+	public static void main (String [] args) {
+		
+		// size of ArrayList 
+        int n = 8; 
+  
+        //declaring ArrayList with initial size n 
+        ArrayList<String> list = new ArrayList<String>(n); 
+		
+		
+  
+		
+	}
+	
+	
+}
