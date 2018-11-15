f95bc72f84eaab3c3ebe72d4a69b7ec915b107f3
Natasha S Love <debora@inbox.ru>
2018-11-14 18:19:07 -0600

    working

--- BadSubscriptCaught.java
+++ BadSubscriptCaught.java
@@ -5,14 +5,36 @@ public class BadSubscriptCaught {
 	public static void main (String [] args) {
 		
 		// size of ArrayList 
-        int n = 8; 
+        //int n = 8; 
+		int answer;
   
         //declaring ArrayList with initial size n 
-        ArrayList<String> list = new ArrayList<String>(n); 
-		
-		
+        ArrayList<String> list = new ArrayList<String>(
+			
+			 Arrays.asList("Mary", 
+                          "Daysha", 
+                          "John",
+						  "Peter",
+						  "Nancy",
+						  "Ann",
+						  "Sonya",
+						  "Bonny")); 
   
+				
+		Scanner inputDevice= new Scanner(System.in);
 		
+		try
+		{
+		System.out.println("Hello, please type an integer:");
+		
+		answer =inputDevice.nextInt();
+		
+		System.out.print(list.get(answer)+" "); 
+		}
+		catch (IndexOutOfBoundsException e)
+		{
+			System.out.println("Error: number should be less than 8.");
+		}
 	}
 	
 	
